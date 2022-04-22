a0cb19528b56c91548699625959999e5f777ef2c
noraroyo <noraroyo@opendeusto.es>
2022-04-22 19:30:17 +0200

    1st commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module strava_client_SD {
+	exports es.deusto.ingenieria.sd.auctions.client.remote;
+	exports es.deusto.ingenieria.sd.auctions.client;
+	exports es.deusto.ingenieria.sd.auctions.client.gui;
+	exports es.deusto.ingenieria.sd.auctions.client.controller;
+
+	requires java.rmi;
+	requires org.apache.commons.codec;
+}
